8c731143949ac75ef110680f297143703943200a
unknown <rodrigo.m.esteves06@gmail.com>
2023-06-10 18:30:47 +0100

    commit everything

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+    <title>Welcome</title>
+</head>
+<body>
+    @include('header')
+
+    <div class="container">
+        <h1>Welcome to bla bla bla</h1>
+    </div>
+
+    <!-- Additional JavaScript files -->
+    <script src="{{ asset('js/app.js') }}"></script>
+</body>
+</html>
